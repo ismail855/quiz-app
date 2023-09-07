92d49ced6ea6f10b43ffe413cbbbace94dc00b45
Muhammad Ismail <60378436+ismail855@users.noreply.github.com>
2023-09-07 17:54:48 +0600

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ export default function Video({ title, id, noq, image }) {
   return (
     <div className={classes.video}>
       <img
-        src={`${import.meta.env.VITE_REACT_PUBLIC_PATH}/images/course/${image}`}
+        src={`${window.location.origin}/images/course/${image}`}
         alt={title}
       />
       <p>{title}</p>
